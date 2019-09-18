ce67a50d62c87ec6626cff65010b203d580b44df
juninhocb <juninhocb@hotmail.com>
2019-09-18 12:15:08 -0300

    atualizacao

--- Teste/tcp-client.c
+++ Teste/tcp-client.c
@@ -1,5 +1,4 @@
 
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -15,14 +14,29 @@ void die(char *s){
     perror(s);
     exit(1);
 }
+
+char* recvBuff(int s, char *msg) {
+int pos = 0;
+while (1) {
+ recv(s, &msg[pos], 1, 0);
+ if (!msg[pos]){
+  break;
+ }
+ pos++;
+}
+return msg;
+}
  
 
 int main(int argc, char *argv[])
 {
     struct sockaddr_in serv_addr; 
-    int s = 0;
+    int s = 0, a=0; 
     char recvBuff[512];
     char sendBuff[512];
+    char sendBuff1[512];
+    //char recvBuff1[512];
+
 
     if(argc != 2)
     {
@@ -61,23 +75,72 @@ int main(int argc, char *argv[])
 	
 
     while (1){
+        //menu
+        printf("1- Enviar mesangem a todos os participantes \n");
+        printf("2- Enviar mesangem privada \n");
+        printf("3- Listar participantes \n");
+        printf("4- Sair \n");
+        scanf("%d", &a);
+       
         
         
-        printf("Enter message : ");
-        fgets(sendBuff, sizeof(sendBuff), stdin);
+        switch(a){
+            
+            case 1: 
+                //zera os buffers
+                memset(sendBuff, 0, sizeof(sendBuff));
+                memset(recvBuff, 0,sizeof(recvBuff));
+                memset(sendBuff1, 0, 512); 
         
-          if (send(s, sendBuff, strlen(sendBuff) , 0)==-1)
-        { 
-            die("send()");
+                //envia 101 ao servidor
+                strcpy(&sendBuff[0], "101"); 
+                send(s, sendBuff, strlen(sendBuff) , 0);
+                
+                sleep(10);
+                
+                //recebe a resposta do servidor
+                recv(s, recvBuff, 512, 0);
+             
+                   
+                int t;   
+                if (strcmp(recvBuff, "105") == 0){
+                printf("Enter message:  \n");
+                scanf("%d", &t);
+                fgets(sendBuff1, sizeof(sendBuff1), stdin);
+                
+                send(s, sendBuff1, strlen(sendBuff1) , 0);
+                
+                    
+                }else {
+                    printf("marco");
+                
+                }
+                
+                fflush(stdin);
+                
+                break;
+            
+            case 4:
+                memset(sendBuff, 0, 512);
+                sendBuff[0] = 104;
+                if (send(s, sendBuff, strlen(sendBuff) , 0)==-1)
+                { 
+                    die("send()");
+                    
+                }
+                
+                 if (recv(s, recvBuff, 512, 0) == -1){
+                    die("recv()");
+                }
+                
+                break;
+            
         }
+            
         
-        memset(recvBuff, 0, 512);
         
-        if (recv(s, recvBuff, 512, 0) == -1){
-            die("recv()");
-        }
-         
-        puts(recvBuff);
+        
+    
         
         
         }
--- Teste/tcp-server.c
+++ Teste/tcp-server.c
@@ -9,64 +9,112 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h> 
+#include <pthread.h>
+
+char* recvBuff(int s, char *msg) {
+int pos = 0;
+while (1) {
+ recv(s, &msg[pos], 1, 0);
+ if (!msg[pos]){
+  break;
+ }
+ pos++;
+}
+return msg;
+}
 
 void die(char *s) {
     perror(s);
     exit(1);
 }
 
+struct client_data{
+    int sk;
+    struct sockaddr_in *client_addr;
+    
+};
+
+void * client_handle(void* cd){
+    struct client_data *client = (struct client_data *)cd;
+    char sendBuff[512];
+    char recvBuff[512]; 
+    char recvBuff1[512];
+    
+    
+    //zera os buffers
+    memset(sendBuff, 0, sizeof(sendBuff)); 
+    memset(recvBuff, 0,sizeof(recvBuff));
+    memset(recvBuff1, 0,sizeof(recvBuff1));
+
+    //strcpy( &sendBuff[0],"Bem vindo" );
+    //send(client->sk, sendBuff, strlen(sendBuff)+1, 0);
+    
+    //quando o cliente se conectar ao servidor, imprime as frases abaixo
+    printf("Received connection from %s:%d\n", inet_ntoa(client->client_addr->sin_addr), ntohs(client->client_addr->sin_port));
+    printf("Waiting for data...");
+    fflush(stdout);
+    
+    //recebe o 1 dado do cliente
+    recv(client->sk, recvBuff, sizeof(recvBuff), 0);
+    
+    printf(" \n Received packet from %s:%d\n", inet_ntoa(client->client_addr->sin_addr), ntohs(client->client_addr->sin_port));
+ 
+   
+    
+    if(strcmp(recvBuff , "101") == 0){
+	
+	 printf("105");
+	 strcpy(&sendBuff[0], "105");
+	 send(client->sk, sendBuff, strlen(sendBuff), 0);
+    
+    }else  { 
+      
+       printf("Comando não encontrado");
+    }	
+	
+    recv(client->sk, recvBuff1, 512, 0);
+    puts(recvBuff1);
+    
+    close(client->sk);
+    
+    free(client->client_addr);
+    free(client);
+
+    return NULL;
+}
+
 int main(int argc, char *argv[])
 {
     struct sockaddr_in serv_addr; 
-    struct sockaddr_in client_addr;
-    unsigned int addrlen = sizeof(client_addr);
-    int s = 0, connfd = 0;
-    char recvBuff[512]; 
-    char sendBuff[512];
+    unsigned int addrlen;
+    int s = 0;
+    struct client_data *cd;
+    pthread_t thr;
 
-    /* Cria o Socket: SOCK_STREAM = TCP */
     s = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, 0, sizeof(serv_addr));
-    memset(sendBuff, 0, sizeof(sendBuff));
-    memset(recvBuff, 0,sizeof(recvBuff)); 
+ 
 
-	/* Configura servidor para receber conexoes de qualquer endereço:
-	 * INADDR_ANY e ouvir na porta 5000 */ 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(5000); 
 
-	/* Associa o socket a estrutura sockaddr_in */
     bind(s, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
 
 	/* Inicia a escuta na porta */
     listen(s, 10); 
 
     while(1) {
-		/* Aguarda a conexão */	
-        connfd = accept(s, (struct sockaddr*)&client_addr, &addrlen); 
-	strcpy( &sendBuff[0],"Bem vindo" );
-	send(connfd, sendBuff, strlen(sendBuff)+1, 0);
+    
+    cd = (struct client_data *)malloc(sizeof(struct client_data));
+    cd->client_addr = (struct sockaddr_in*)malloc(sizeof(struct sockaddr_in));
+    addrlen = sizeof(struct sockaddr_in);
 
-		/* Imprime IP e porta do cliente. */
-        printf("Received connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-	printf("Waiting for data...");
-        fflush(stdout);
-        
-	 if (recv(connfd, recvBuff, 512, 0) == -1){
-           die("recv()");
-        }
-	
-	printf("Received packet from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-        printf("Data: %s\n" , recvBuff);
+    cd->sk = accept(s, (struct sockaddr*)cd->client_addr, (socklen_t*)&addrlen); 
+
+    pthread_create(&thr, NULL, client_handle, (void *)cd);
+    pthread_detach(thr);
 	
 	
-	if (send(connfd, recvBuff, strlen(recvBuff), 0) == -1){             
-           die("send()");        
-        }
-	
-	puts(recvBuff);
-}
-     close(s);
-     return 0;
+     }
 }
